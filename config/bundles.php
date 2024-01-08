13aecce8f8dc8bacde4c34d4510a37ea984474fd
Julie <raymondjulie@proton.me>
2024-01-08 11:03:47 +0100

    adding ux-live-component, and SearchBar component initialized

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
