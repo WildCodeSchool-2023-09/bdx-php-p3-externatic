fd7a740f10376f93c06f10f190695d6f32bb0133
Martha Smith <m.e.smith@live.co.uk>
2024-01-10 14:44:38 +0100

    added class to navbar and footer

--- migrations/Version20240110100333.php
+++ migrations/Version20240110100333.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240109100134 extends AbstractMigration
+final class Version20240110100333 extends AbstractMigration
 {
     public function getDescription(): string
     {
