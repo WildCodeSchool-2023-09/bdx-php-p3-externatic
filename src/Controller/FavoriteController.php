1e78b152cde7b8b971e415218e7e5c01c46affd2
Julie <raymondjulie@proton.me>
2024-01-15 10:20:12 +0100

    favorite route to show candidates favorites of a company working

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CandidateRepository;
 use App\Repository\JobRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,6 +21,20 @@ class FavoriteController extends AbstractController
 
         return $this->render('favorite/favorite_jobs.html.twig', [
             'favoriteJobs' => $favoriteJobs,
+        ]);
+    }
+
+    #[Route('/favorite/candidates', name: 'app_favorite_candidates')]
+    public function favoriteCandidates(CandidateRepository $candidateRepository): Response
+    {
+        // Récupère l'utilisateur actuel
+        $user = $this->getUser();
+
+        // Récupère la liste des candidats favoris pour l'utilisateur avec le rôle COMPANY
+        $favoriteCandidates = $candidateRepository->findByLikedCompany($user);
+
+        return $this->render('favorite/favorite_candidates.html.twig', [
+            'favoriteCandidates' => $favoriteCandidates,
         ]);
     }
 }
