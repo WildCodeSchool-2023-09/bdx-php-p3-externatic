9e3ccd5bbf1cf103a8aa7be8d2f93027449fe2ff
Martha Smith <m.e.smith@live.co.uk>
2024-01-24 14:55:12 +0100

    able to view applications per job

--- src/Controller/ApplyController.php
+++ src/Controller/ApplyController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\FileUploader;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 
 class ApplyController extends AbstractController
 {
