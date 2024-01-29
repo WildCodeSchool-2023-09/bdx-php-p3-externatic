c69fde3ba118ede7b31575d08cb5c11764c7f3b9
Melprcllr <m.parceiller@free.fr>
2024-01-29 23:14:19 +0100

    add css form new job offer

--- src/Form/JobType.php
+++ src/Form/JobType.php
@@ -7,25 +7,59 @@ use App\Entity\Company;
 use App\Entity\Job;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class JobType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('adress')
-            ->add('description')
-            ->add('startDate')
-            ->add('salary')
-            ->add('city')
-//           ->add('company', EntityType::class, [
-//                'class' => Company::class,
-//                'choice_label' => 'id',
-              /*  'disabled' => true, // Make the field disabled so it cannot be edited*/
-//            ])
+            ->add('title', TextType::class, [
+                'label' => false,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 2, 'max' => 255]),
+                ],
+            ])
+            ->add('adress', TextType::class, [
+                'label' => false,
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+            ->add('description', TextType::class, [
+                'label' => false,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 2, 'max' => 255]),
+                ],
+            ])
+            ->add('startDate', null, [
+                'label' => false,
+                             ]
+            )
+            ->add('salary', TextType::class, [
+                'label' => false,
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+            ->add('city', TextType::class, [
+                'label' => false,
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+             ->add('company', EntityType::class, [
+                   'class' => Company::class,
+                    'label' => false,
+                    'choice_label' => 'id',
+                    'disabled' => true, // Désactive le champ pour qu'il ne puisse pas être modifié*/
+            ])
         ;
     }
 
